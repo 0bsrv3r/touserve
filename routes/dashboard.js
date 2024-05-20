c1df11a60a40f04d1a6cc5ec2c01c53e635dcd24
0bsrv3r <ragim1753@gmail.com>
2024-05-20 17:26:41 +0400

    Added U-Bold dashboard page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,7 @@ const login = require("../middlewares/auth.js")
 router.use(express.urlencoded({ extended:true }))
 
 router.get('/', login, (req, res) => { 
-    res.render("dashboard", {layout: false}); 
+    res.render("./dashboard/dashboard", {layout: 'layouts/dashboard/top-side-bars'}); 
 }); 
 
 module.exports = router;
