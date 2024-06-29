fae8d849b9ce81485d6b7c8b6db07e84002eb6ea
0bsrv3r <ragim1753@gmail.com>
2024-06-30 03:36:59 +0400

    edited star-rate part of review with css and js

---
+++ upload/javascript/star-rate.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var stars = document.querySelectorAll("#star-rate .star.v1 li");
+    var inputField = document.getElementById("star-value");
+
+    stars.forEach(function(star) {
+        star.addEventListener("mouseover", function() {
+            this.classList.add("active");
+            let prevSibling = this.previousElementSibling;
+            while (prevSibling) {
+                prevSibling.classList.add("active");
+                prevSibling = prevSibling.previousElementSibling;
+            }
+        });
+
+        star.addEventListener("mouseout", function() {
+            this.classList.remove("active");
+            let prevSibling = this.previousElementSibling;
+            while (prevSibling) {
+                prevSibling.classList.remove("active");
+                prevSibling = prevSibling.previousElementSibling;
+            }
+        });
+
+        star.addEventListener("click", function() {
+            this.classList.add("keep-active");
+            let prevSibling = this.previousElementSibling;
+            while (prevSibling) {
+                prevSibling.classList.add("keep-active");
+                prevSibling = prevSibling.previousElementSibling;
+            }
+
+            let nextSibling = this.nextElementSibling;
+            while (nextSibling) {
+                nextSibling.classList.remove("keep-active");
+                nextSibling = nextSibling.nextElementSibling;
+            }
+
+            let starValue = this.getAttribute("data-value");
+            inputField.value = starValue;
+        });
+    });
+});
