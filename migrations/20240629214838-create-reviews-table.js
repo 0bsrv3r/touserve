0f24036f8c9d2360baf7168686a6abd318e9d63a
0bsrv3r <ragim1753@gmail.com>
2024-06-30 02:03:15 +0400

    extracted review components and created review model & middleware files

---
+++ migrations/20240629214838-create-reviews-table.js
@@ -0,0 +1,76 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {       // foreign key
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      tourId: {       // foreign key
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Tours', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      accommodationId: {       // foreign key
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Accommodations', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      guideId: {       // foreign key
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Customers', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      review: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      stars: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reviews');
+  }
+};
---
+++ models/reviews.js
@@ -0,0 +1,71 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Reviews extends Model {
+    static associate(models) {
+      // Association definition
+      Reviews.belongsTo(models.Users, { foreignKey: 'userId', as: 'users' });
+      Reviews.belongsTo(models.Tours, { foreignKey: 'tourId', as: 'tours' });
+      Reviews.belongsTo(models.Accommodations, { foreignKey: 'accommodationId', as: 'accommodations' });
+      Reviews.belongsTo(models.Customers, { foreignKey: 'guideId', as: 'guides' });
+    }
+  }
+  Reviews.init({
+    userId: {       // foreign key
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+    tourId: {       // foreign key
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Tours', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+    accommodationId: {       // foreign key
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Accommodations', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+    guideId: {       // foreign key
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Customers', // name of the target model
+          key: 'id',      // key in the target model
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+    review: {
+        allowNull: false,
+        type: DataTypes.TEXT
+    },
+    stars: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+    },
+  }, {
+    sequelize,
+    modelName: 'Reviews',
+    tableName: 'Reviews',
+    timestamps: true
+  });
+
+  return Reviews;
+};
