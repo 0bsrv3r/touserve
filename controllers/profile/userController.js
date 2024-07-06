c4ffe2fca3dc06fc07129cfcad63535d959a7087
0bsrv3r <ragim1753@gmail.com>
2024-07-06 18:51:44 +0400

    Developed user update email,number,pasword and general info functionality

--- controllers/profile/userController.js
+++ controllers/profile/userController.js
@@ -43,6 +43,112 @@ class Profile{
         }
         
     }
+
+    static async updateEmail(req, res){
+        const errors = validationResult(req)
+        
+        if(errors.isEmpty()){
+            const {email} = req.body;
+            const id = 1; // req.session.user_id //UPDATE THIS IN PROD ENV
+
+            const user = await Users.findOne({where: id})
+
+            if(user){
+                user.email = email
+                await user.save()
+            }
+            return res.redirect('/user/profile')
+        }else{
+            const errorObject = errors.array().reduce((acc, error) => {
+                acc[error.path] = error.msg;
+                return acc;
+            }, {})
+            return res.json(errorObject)
+        }
+    }
+
+    static async updateNumber(req, res){
+        const errors = validationResult(req)
+        
+        if(errors.isEmpty()){
+            const {number} = req.body;
+            const id = 1; // req.session.user_id //UPDATE THIS IN PROD ENV
+
+            const user = await Users.findOne({where: id})
+
+            if(user){
+                user.number = number
+                await user.save()
+            }
+            return res.redirect('/user/profile')
+        }else{
+            const errorObject = errors.array().reduce((acc, error) => {
+                acc[error.path] = error.msg;
+                return acc;
+            }, {})
+            return res.json(errorObject)
+        }
+    }
+
+    static async updatePassword(req, res){
+        const errors = validationResult(req)
+        
+        if(errors.isEmpty()){
+            const {currentPass, newPass1} = req.body
+            const id = 1 //req.session.user_id //UPDATE THIS IN PROD ENV
+
+            const user = await Users.findOne({where: {id, password:currentPass}})
+
+            if(user){
+                user.password = newPass1
+                await user.save()
+            }
+            return res.redirect('/user/profile')
+        }else{
+            const errorObject = errors.array().reduce((acc, error) => {
+                acc[error.path] = error.msg;
+                return acc;
+            }, {})
+
+            return res.json(errorObject)
+        }    
+    }
+
+    static async updateGeneral(req, res){
+        const errors = validationResult(req)
+        
+        if(errors.isEmpty()){
+            const id = 1 //req.session.user_id //UPDATE THIS IN PROD ENV
+            const user = await Users.findOne({where: {id}})
+
+            if(user){
+                const data = {
+                   firstName: req.body.firstName, 
+                   lastName: req.body.lastName,
+                   age: req.body.age,
+                   experience: req.body.experience, 
+                   languages: req.body.languages, 
+                   description: req.body.description
+                }
+
+                Object.keys(data).forEach(key => {
+                    if (data[key]) {
+                        user[key] = data[key];
+                    }
+                });
+
+                await user.save();
+            }
+            return res.redirect('/user/profile')
+        }else{
+            const errorObject = errors.array().reduce((acc, error) => {
+                acc[error.path] = error.msg;
+                return acc;
+            }, {})
+
+            return res.json(errorObject)
+        }    
+    }
 }
 
 module.exports = Profile
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,17 @@ const express  = require('express');
 const router = express.Router();
 const User = require("../controllers/profile/userController.js")
 const profileValidation  = require("../validators/image-update.js"); 
+const userValidation  = require("../validators/customer-user-update.js"); 
 
 router.use(express.urlencoded({ extended:true }))
 
 router.get('/profile', User.getProfileInfo);
 router.post('/profile/photo', profileValidation, User.uploadProfilePhoto);
+
+// Update Personal Information
+router.post('/update/email', userValidation, User.updateEmail)
+router.post('/update/number', userValidation, User.updateNumber)
+router.post('/update/password', userValidation,  User.updatePassword)
+router.post('/update/general', userValidation,  User.updateGeneral)
 
 module.exports = router
