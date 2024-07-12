bdaea7b647171756a59e43b28a7877abc8c5db24
0bsrv3r <ragim1753@gmail.com>
2024-07-12 17:20:22 +0400

    Updated products star rate for the index page.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,13 @@
 const {Tours, Accommodations, Events, Customers} = require('./../models')
+const Entities = require('./../services/modelService')
 
 class Index{
     static async allServices(req, res){
-        const tours  =  await Tours.findAll()
-        const accommodations  =  await Accommodations.findAll()
-        const guides  =  await Customers.findAll({where: {role:"guide", verifiedAt: true}})
-
-        return res.render("index", {
-            layout: 'layouts/header', 
-            tours: tours, 
-            accommodations: accommodations, 
-            guides: guides})
+        const tours  =  await Entities.getEntities(Tours, "tourId", {order: 'totalStars DESC', limit:5})
+        const accommodations  =  await Entities.getEntities(Accommodations, "accommodationId", {order: 'totalStars DESC', limit:5})
+        const guides  =  await Entities.getEntities(Customers, 'guideId', {where: {role:"guide", verifiedAt: true}, order: 'totalStars DESC', limit:5})
+        
+        return res.render("index", {layout: 'layouts/header', tours: tours, accommodations: accommodations, guides: guides})
     }
 }
 
