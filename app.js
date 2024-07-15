9e440922f89c4ee906eb95ae286f67a4be742bf4
0bsrv3r <ragim1753@gmail.com>
2024-07-15 19:33:16 +0400

    Tested cookie-session in prod with different configuration

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(session({
         // sameSite: 'strict',
         // name: 'session',
         // secret: process.env.SESSION_KEY, 
-        // resave: false, 
-        // saveUninitialized: false,
+        resave: false, 
+        saveUninitialized: false,
         cookie: {
             maxAge: 24 * 60 * 60 * 1000,
             secure: true,
